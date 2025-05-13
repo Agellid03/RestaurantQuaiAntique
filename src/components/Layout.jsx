31891a174d9f4c98b39bcdade0a2e1373567101e
Agellid03 <ouldbrahamsofiane@gmail.com>
2025-05-13 18:10:32 +0200

    Stylisation du footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '../components/header/Header';
+import Header from './header/Header';
 import Footer from './footer/Footer';
 
 const Layout = () => {
