ccd8762b745b0327c7cb3cb115370b12450ea4b8
Agellid03 <ouldbrahamsofiane@gmail.com>
2025-05-18 15:21:28 +0200

    Creation section presentation et stylisation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,27 @@ import '../styles/home.css';
 
 const Home = () => {
   return (
-    <section className="hero">
-      <h2 className="hero-title">Bienvenue au Quai Antique</h2>
-      <p>
-        L’élégance du goût, l’âme de la cuisine française Découvrez une cuisine
-        raffinée et authentique, entre tradition et créativité, dans un cadre
-        chaleureux et soigné.
-      </p>
-      <Link to="/reservation" className="cta-reserver">
-        Reserver
-      </Link>
-    </section>
+    <>
+      <section className="hero">
+        <h2 className="hero-title">Bienvenue au Quai Antique</h2>
+        <p>Le chef Arnaud Michant vous invite Quai Antique</p>
+        <Link to="/reservation" className="cta-reserver">
+          Reserver
+        </Link>
+      </section>
+
+      <section className="presentation">
+        <img src="/images/Presentation.jpg" alt="Présentation du restaurant" />
+        <div className="presentation-text">
+          <h3>Une cuisine de caractère</h3>
+          <p>
+            L’élégance du goût, l’âme de la cuisine française. Découvrez une
+            cuisine raffinée et authentique, entre tradition et créativité, dans
+            un cadre chaleureux et soigné.
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 
